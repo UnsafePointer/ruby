cee1675cdb23b7caca18255335f9e3a67d9f5bd3
Renzo Crisostomo <renzo@crisostomo.me>
2019-01-23 23:26:17 +0100

    Implement initial loop in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include "BIOS.hpp"
+#include "Interconnect.hpp"
+#include "CPU.hpp"
 
 int main() {
     BIOS bios = BIOS();
     bios.loadBin("SCPH1001.BIN");
+    Interconnect interconnect = Interconnect(bios);
+    CPU cpu = CPU(interconnect);
+    while (true) {
+        cpu.executeNext();
+    }
 }
