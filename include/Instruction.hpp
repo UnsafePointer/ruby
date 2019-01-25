968639a0d4bd3355c0f56fdae918767467690d78
Renzo Crisostomo <renzo@crisostomo.me>
2019-01-25 21:07:33 +0100

    Add more MIPS Assembly instruction format members to Instruction struct

--- include/Instruction.hpp
+++ include/Instruction.hpp
@@ -7,6 +7,9 @@ struct Instruction {
     Instruction(uint32_t data);
     ~Instruction();
     uint32_t funct() const;
+    uint32_t rs() const;
     uint32_t rt() const;
+    uint32_t rd() const;
     uint32_t imm() const;
+    uint32_t immSE() const;
 };
--- src/Instruction.cpp
+++ src/Instruction.cpp
@@ -12,10 +12,23 @@ uint32_t Instruction::funct() const {
     return data >> 26;
 }
 
+uint32_t Instruction::rs() const {
+    return (data >> 21) & 0x1f;
+}
+
 uint32_t Instruction::rt() const {
     return (data >> 16) & 0x1F;
 }
 
+uint32_t Instruction::rd() const {
+    return (data >> 11) & 0x1f;
+}
+
 uint32_t Instruction::imm() const {
     return data & 0xFFFF;
+}
+
+uint32_t Instruction::immSE() const {
+    int16_t signedData = data & 0xFFFF;
+    return (uint32_t)signedData;
 }
