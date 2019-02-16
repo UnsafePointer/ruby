e546f5654f846a5d096a20725690f0276eefc299
Renzo Crisostomo <renzo@crisostomo.me>
2019-02-16 17:09:55 +0100

    Add CPU register and COP0 register summaries

--- include/CPU.hpp
+++ include/CPU.hpp
@@ -3,6 +3,42 @@
 #include "Instruction.hpp"
 #include "RegisterIndex.hpp"
 
+/*
+CPU Register Summary
+Name       Alias    Common Usage
+(R0)       zero     Constant (always 0) (this one isn't a real register)
+R1         at       Assembler temporary (destroyed by some pseudo opcodes!)
+R2-R3      v0-v1    Subroutine return values, may be changed by subroutines
+R4-R7      a0-a3    Subroutine arguments, may be changed by subroutines
+R8-R15     t0-t7    Temporaries, may be changed by subroutines
+R16-R23    s0-s7    Static variables, must be saved by subs
+R24-R25    t8-t9    Temporaries, may be changed by subroutines
+R26-R27    k0-k1    Reserved for kernel (destroyed by some IRQ handlers!)
+R28        gp       Global pointer (rarely used)
+R29        sp       Stack pointer
+R30        fp(s8)   Frame Pointer, or 9th Static variable, must be saved
+R31        ra       Return address (used so by JAL,BLTZAL,BGEZAL opcodes)
+-          pc       Program counter
+-          hi,lo    Multiply/divide results, may be changed by subroutines
+
+COP0 Register Summary
+cop0r0-r2   - N/A
+cop0r3      - BPC - Breakpoint on execute (R/W)
+cop0r4      - N/A
+cop0r5      - BDA - Breakpoint on data access (R/W)
+cop0r6      - JUMPDEST - Randomly memorized jump address (R)
+cop0r7      - DCIC - Breakpoint control (R/W)
+cop0r8      - BadVaddr - Bad Virtual Address (R)
+cop0r9      - BDAM - Data Access breakpoint mask (R/W)
+cop0r10     - N/A
+cop0r11     - BPCM - Execute breakpoint mask (R/W)
+cop0r12     - SR - System status register (R/W)
+cop0r13     - CAUSE - (R)  Describes the most recently recognised exception
+cop0r14     - EPC - Return Address from Trap (R)
+cop0r15     - PRID - Processor ID (R)
+cop0r16-r31 - Garbage
+cop0r32-r63 - N/A - None such (Control regs)
+*/
 class CPU {
     uint32_t programCounter;
     Instruction nextInstruction;
