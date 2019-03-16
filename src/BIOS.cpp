b43f4a21da37cf1259c49cb64ae122b29270cf7d
Renzo Crisostomo <renzo@crisostomo.me>
2019-03-16 17:41:20 +0100

    Add error message to BIOS file load method

--- src/BIOS.cpp
+++ src/BIOS.cpp
@@ -1,5 +1,6 @@
 #include "BIOS.hpp"
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ BIOS::~BIOS() {
 void BIOS::loadBin(const string& path) {
     ifstream file (path, ios::in|ios::binary|ios::ate);
     if (!file.is_open()) {
+        cout << "Unable to load BIOS" << endl;
         exit(1);
     }
     streampos size = file.tellg();
