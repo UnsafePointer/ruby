23b15aad0c44aa8d538b91cf7408103254748e82
Renzo Crisostomo <renzo@crisostomo.me>
2019-04-25 23:27:34 +0200

    Fix texture sampling in fragment shader (transparency)
    Color 0x0 in the PlayStation texture format means transparent

--- glsl/fragment.glsl
+++ glsl/fragment.glsl
@@ -14,6 +14,10 @@ const uint BLEND_MODE_NO_TEXTURE = 0U;
 const uint BLEND_MODE_RAW_TEXTURE = 1U;
 const uint BLEND_MODE_TEXTURE_BLEND = 2U;
 
+bool is_transparent(vec4 texel) {
+  return texel == vec4(0U, 0U, 0U, 0U);
+}
+
 void main() {
     if (fragment_texture_blend_mode == BLEND_MODE_NO_TEXTURE) {
         fragment_color = vec4(color, 1.0);
@@ -27,6 +31,12 @@ void main() {
 
         texel_x_pix += fragment_texture_page.x;
         texel_y += fragment_texture_page.y;
-        fragment_color = texelFetch(frame_buffer_texture, ivec2(texel_x_pix, texel_y), 0);
+        vec4 texel = texelFetch(frame_buffer_texture, ivec2(texel_x_pix, texel_y), 0);
+
+        if (is_transparent(texel)) {
+            discard;
+        }
+
+        fragment_color = texel;
     }
 }
