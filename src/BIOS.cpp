0b2b9bb16dc17735232ba3b84887a246eaaa1302
Renzo Crisostomo <renzo@crisostomo.me>
2019-07-03 21:20:03 +0200

    Move BIOS binary read method to helper for future reuse

--- src/BIOS.cpp
+++ src/BIOS.cpp
@@ -1,6 +1,6 @@
 #include "BIOS.hpp"
-#include <fstream>
 #include "Output.hpp"
+#include "Helpers.hpp"
 
 using namespace std;
 
@@ -13,12 +13,5 @@ BIOS::~BIOS() {
 }
 
 void BIOS::loadBin(const string& path) {
-    ifstream file (path, ios::in|ios::binary|ios::ate);
-    if (!file.is_open()) {
-        printError("Unable to load BIOS");
-    }
-    streampos size = file.tellg();
-    file.seekg(0, ios::beg);
-    file.read(reinterpret_cast<char *>(data), size);
-    file.close();
+    readBinary(path, data);
 }
