0fc1cdc1cc82cf22178ed734c16481c174708e07
Renzo Crisostomo <renzo@crisostomo.me>
2019-10-18 00:18:55 +0200

    Add readBinary helper with position offset

--- src/Helpers.cpp
+++ src/Helpers.cpp
@@ -5,16 +5,20 @@
 
 using namespace std;
 
-void readBinary(const string& path, uint8_t *data) {
-    ifstream file = ifstream(path, ios::in|ios::binary|ios::ate);
+void readBinary(const std::string& path, uint8_t *data, uint32_t atOrigin) {
+    ifstream file = ifstream(path, ios::in | ios::binary | ios::ate);
     if (!file.is_open()) {
         cout << format("Unable to load binary at path %s", path.c_str()) << endl;
         exit(1);
     }
     streampos size = file.tellg();
-    file.seekg(0, ios::beg);
+    file.seekg(atOrigin, ios::beg);
     file.read(reinterpret_cast<char *>(data), size);
     file.close();
+}
+
+void readBinary(const string& path, uint8_t *data) {
+    readBinary(path, data, 0);
 }
 
 uint8_t decimalFromBCDEncodedInt(uint8_t bcdEncoded) {
--- src/RAM.cpp
+++ src/RAM.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <fstream>
 #include "Output.hpp"
-#include <iostream>
+#include "Helpers.hpp"
 
 using namespace std;
 
@@ -14,14 +14,8 @@ RAM::~RAM() {
 }
 
 void RAM::receiveTransfer(std::string path, uint32_t origin, uint32_t size, uint32_t destination) {
-    ifstream file (path, ios::in|ios::binary|ios::ate);
-    if (!file.is_open()) {
-        cout << "Unable to load binary" << endl;
-    }
-    file.seekg(origin);
     uint8_t *dataDestination = &data[destination];
-    file.read(reinterpret_cast<char *>(dataDestination), size);
-    file.close();
+    readBinary(path, dataDestination, origin);
 }
 
 void RAM::dump() {
