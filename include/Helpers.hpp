f56e0783365e0960b7e9f533848b30b11b5f7b87
Renzo Crisostomo <renzo@crisostomo.me>
2019-10-18 23:28:44 +0200

    Fix incorrect EXE load size

--- include/Helpers.hpp
+++ include/Helpers.hpp
@@ -2,6 +2,6 @@
 #include <string>
 #include <cstdint>
 
-void readBinary(const std::string& path, uint8_t *data, uint32_t atOrigin);
+void readBinary(const std::string& path, uint8_t *data, uint32_t atOrigin, int64_t size);
 void readBinary(const std::string& path, uint8_t *data);
 uint8_t decimalFromBCDEncodedInt(uint8_t bcdEncoded);
--- src/Helpers.cpp
+++ src/Helpers.cpp
@@ -5,20 +5,25 @@
 
 using namespace std;
 
-void readBinary(const std::string& path, uint8_t *data, uint32_t atOrigin) {
+void readBinary(const std::string& path, uint8_t *data, uint32_t atOrigin, int64_t size) {
     ifstream file = ifstream(path, ios::in | ios::binary | ios::ate);
     if (!file.is_open()) {
         cout << format("Unable to load binary at path %s", path.c_str()) << endl;
         exit(1);
     }
-    streampos size = file.tellg();
+    unsigned long long sizeToRead;
+    if (size == -1) {
+        sizeToRead = file.tellg();
+    } else {
+        sizeToRead = size;
+    }
     file.seekg(atOrigin, ios::beg);
-    file.read(reinterpret_cast<char *>(data), size);
+    file.read(reinterpret_cast<char *>(data), sizeToRead);
     file.close();
 }
 
 void readBinary(const string& path, uint8_t *data) {
-    readBinary(path, data, 0);
+    readBinary(path, data, 0, -1);
 }
 
 uint8_t decimalFromBCDEncodedInt(uint8_t bcdEncoded) {
