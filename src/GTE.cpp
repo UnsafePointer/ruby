39750a50912b3904e9b68d1b2e4174edfec590b6
Renzo Crisostomo <renzo@crisostomo.me>
2020-04-03 20:42:22 +0200

    Implement member initialization in GTE

--- src/GTE.cpp
+++ src/GTE.cpp
@@ -1,7 +1,50 @@
 #include "GTE.hpp"
 #include "GTEInstruction.hpp"
 
-GTE::GTE(LogLevel logLevel) : logger(logLevel, "  GTE: ") {}
+GTE::GTE(LogLevel logLevel) : logger(logLevel, "  GTE: "),
+    v0({0, 0, 0}),
+    v1({0, 0, 0}),
+    v2({0, 0, 0}),
+    rgbc(GTEColor_u8_t()),
+    otz(0),
+    ir0(0),
+    ir1(0),
+    ir2(0),
+    ir3(0),
+    sxy0({0, 0}),
+    sxy1({0, 0}),
+    sxy2({0, 0}),
+    sxyp({0, 0}),
+    sz0(0),
+    sz1(0),
+    sz2(0),
+    sz3(0),
+    rgb0(GTEColor_u8_t()),
+    rgb1(GTEColor_u8_t()),
+    rgb2(GTEColor_u8_t()),
+    res1(0),
+    mac0(0),
+    mac1(0),
+    mac2(0),
+    mac3(0),
+    irgb(0),
+    orgb(0),
+    lzcs(0),
+    lzcr(0),
+    rt({{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}),
+    tr({0, 0, 0}),
+    l({{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}),
+    bk({0, 0, 0}),
+    lr({{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}),
+    fc({0, 0, 0}),
+    of({0, 0}),
+    hl(0),
+    dqa(0),
+    dqb(0),
+    zsf3(0),
+    zsf4(0)
+{
+}
 
 GTE::~GTE() {}
 
